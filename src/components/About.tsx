3a14146572a9cda6244b3f88e69bd715930cc621
Cipher <vipingiribgb0@gmail.com>
2026-03-21 12:10:23 +0000

    gg

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Full Stack Developer with  cybersecurity focus, building fast, scalable, and secure web applications.
+          Full Stack Developer with a cybersecurity focus, building fast, scalable, and secure web applications.
           I test for vulnerabilities like XSS, IDOR, and logic flaws, and have reported 200+ XSS issues across public and government platforms, including rate limit and CORS misconfigurations.
           I focus on building systems that are both functional and secure from day one.
         </p>
